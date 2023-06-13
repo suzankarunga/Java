c975b1e6c09588aca01f47f11267f24c83ecc2a0
suzankarunga <93705652+suzankarunga@users.noreply.github.com>
2023-06-13 13:04:51 +0300

    Add files via upload

---
+++ leasons.js
@@ -0,0 +1,54 @@
+const { machine } = require("os");
+
+//Arithmetic Operators
+console.log(2+2);//addd
+console.log(2/2);//divide
+console.log(2-2); //subtract
+console.log(2*2); //multiply
+
+
+console.log(10 + 3.5)
+console.log(2022 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
+
+// string concatication
+console.log("hi" +" "+ "jeromi")
+console.log('one' + ',' + 'two' + ',' + 'three !')
+
+//properties
+console.log('I love to code but i hate stress'.length);
+
+//Methods
+console.log('hello'.toUpperCase());
+console.log('Love'.startsWith('L'));
+console.log('Hey'.startsWith('T'));
+
+// Use .toUpperCase() to log 'Amen' in all uppercase letters
+console.log('Amen'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+//Built in functions
+console.log(Math.random());
+console.log(Math.random('from 50'));
+console.log(Math.floor(Math.random()  *20));//evaluates a decimal and gives out the output as awhole number
+console.log(Math.ceil(70)) //returns the smallest number
+console.log(Number.isInteger(70));
+
+
+// Variable
+
+ var ton = 'machine';
+console.log(ton);
+
+
+//using a bolean
+let changeme =true;
+changeme =false;
+console.log(changeme);
+
+
+
+
