2822a9faa120cb713e3d59239f00122581595691
suzankarunga <93705652+suzankarunga@users.noreply.github.com>
2023-06-21 15:21:37 +0300

    Add files via upload

---
+++ jn.js
@@ -0,0 +1,56 @@
+
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+
+console.log("Hello me ts abig thing");
+//Variables//
+let name ='ken' ;
+console.log(name);
+let firstname ='Tatiana' ;
+console.log(firstname);
+let lastname ='Xanner' ;
+console.log(lastname);
+
+//constants
+//let interestRate = 0.3;
+//interestRate =1;
+//console.log(interestRate);
+//let name ='Gemie';//string literal
+//let age =80;// Number literal
+//let isApproved =false;
+//Let firstname = undefined ;
+//let selected colour = null;
+//let selected colour ='red'
+
+  //Objects
+  
+  
+let person ={
+    name: 'jan',
+    age:30
+};
+console.log(person.name ,  person.age);
+//Array
+let selectedColors = ['red' ,'blue'];
+selectedColors[2] = 'green';
+
+console.log(selectedColors.length);
+
+  
+//Functions
+//performing a task
+function greet(name){
+    console.log('Hello '+ name );
+}
+greet('zan');
+greet('smith');
+
+//calculating a task
+function square(number){
+    return number * number;
+}
+
+console.log(square(2));
+
+ 
