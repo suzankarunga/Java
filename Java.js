2984a364123a173c5d93b9defab82cd0f98e93fd
suzankarunga <93705652+suzankarunga@users.noreply.github.com>
2023-06-21 15:20:53 +0300

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,45 @@
+public class HideAndSeek {
+  public static void main(String[] args) {
+    System.out.println("Let's play hide and seek.");
+    System.out.print("Three...");
+    System.out.print("Two...");
+    System.out.println("One...");
+    System.out.println("Ready or not, here I come!");
+  }
+}
+
+public class Timeline {
+  public static void main(String[] args) {
+    System.out.println("Hello Java!");
+    
+    System.out.println("You were born in 1995");
+
+    // Sun Microsystems announced the release of Java in 1995
+    
+    System.out.println("You were created by James Gosling");
+    /*
+		James Gosling is a Canadian engineer who 
+		created Java while working at Sun Microsystems. 
+		His favorite number is the square root of 2!
+    */
+    System.out.println("You are a fun language!");
+  }
+} 
+
+
+ublic class LanguageFacts {
+  public static void main(String[] args) {
+    // Press enter or return on your keyboard after each semicolon!
+    
+    System.out.println("Happy");
+    //Entering a newline after  a semicoloni
+    System.out.println("Dance");
+    System.out.println("Cry");
+  }
+}
+public class Review {  
+  public static void main(String[] args) {
+    // The main method executes the tasks of the class
+    System.out.println("My first Java program from scratch!");
+  } 
+}
